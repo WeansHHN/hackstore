a8954054c3872c290de6c4aefbad2253bc2b24e2
Weans <137310130+WeansHHN@users.noreply.github.com>
2025-10-06 10:12:13 +0700

    Create test.js

---
+++ apiJS/test.js
@@ -0,0 +1,29 @@
+{
+    "game": [
+        {
+            "iconURL": "https://...",
+            "name": "Liên Quân",
+            "version": "1.0",
+            "description": "Game MOBA 5v5",
+            "downloadURL": "https://..."
+        }
+    ],
+    "app": [
+        {
+            "iconURL": "https://ipaomtk.com/wp-content/uploads/2023/03/esign-ipa.jpg",
+            "name": "Esign",
+            "version": "4.8.2",
+            "description": "Ứng dụng sign IPA",
+            "downloadURL": "https://github.com/WeansHHN/troll/releases/download/Trollstoreipa/esign_4.8.2_unsigned.ipa"
+        }
+    ],
+    "troll": [
+        {
+            "iconURL": "https://play-lh.googleusercontent.com/H7li8Hiz2VMst508T5B8IP3g5ivLhjqrKt4PsM4SCZMO8hfGuDQO3pC5hYjnfUT2Jj4=w480-h960",
+            "name": "Voloco",
+            "version": "8.12.2",
+            "description": "App troll chỉnh giọng",
+            "downloadURL": "https://github.com/KevinPhampn/Up-iPA/releases/download/ipa/Voloco.HAHiOS_8.12.2.ipa"
+        }
+    ]
+}
